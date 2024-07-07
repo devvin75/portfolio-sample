782894fd41a58faee13b1cdfc6766a26b1c2650f
Alvin <alvingomez.world@gmail.com>
2024-07-07 08:11:33 +0800

    Framework additional components
    Added the components for the base framework. Rendered Navbar for testing tailwindcss

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <div className="w-screen h-screen bg-orange-300">
+          <Navbar />
+          {children}
+        </div>
+        
       </body>
     </html>
   );
